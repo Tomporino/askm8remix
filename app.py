bcb8f74cb96148b1a71bbe617cdfb4796bdbdd86
Tomporino <tomorpeter97@gmail.com>
2020-03-29 22:20:40 +0200

    add answer

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request, url_for
 import data_handler
+import util
 
 app = Flask(__name__)
 
@@ -10,8 +11,18 @@ def home():
     return render_template('home.html', questions=questions)
 
 
-@app.route('/question/<question_id>')
+@app.route('/question/<question_id>', methods=['POST', 'GET'])
 def question(question_id):
+    if request.method == 'POST':
+        user_answer = {
+            'submission_time': util.get_current_time(),
+            'vote_number': 0,
+            'question_id': question_id,
+            'message': request.form['answer_message'],
+            'image': None
+        }
+        data_handler.add_answer(user_answer)
+        return redirect(request.url)
     question = data_handler.get_selected_question(question_id)
     answers = data_handler.get_answers_for_question(question_id)
     return render_template('question.html', question=question, answers=answers)
--- data_handler.py
+++ data_handler.py
@@ -22,6 +22,14 @@ def get_selected_question(cursor, question_id):
 def get_answers_for_question(cursor, question_id):
     query = """
             SELECT * FROM answer
-            WHERE question_id=%(question_id)s"""
+            WHERE question_id=%(question_id)s;"""
     cursor.execute(query, {'question_id': question_id})
     return cursor.fetchall()
+
+
+@connection.connection_handler
+def add_answer(cursor, details):
+    query = """
+            INSERT INTO answer (submission_time, vote_number, question_id, message, image)
+            VALUES (%(submission_time)s, %(vote_number)s, %(question_id)s, %(message)s, %(image)s)"""
+    cursor.execute(query, details)
