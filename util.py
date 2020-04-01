9f693289dac2313a3bd207e05bee8718e650b057
Tibor Düsüngülü <dusungulu.tibi@gmail.com>
2020-04-02 00:27:47 +0200

    get_valid_questions function added
    Surrounds the search_phrase parameter with 'span' HTML tags

--- util.py
+++ util.py
@@ -3,3 +3,10 @@ from datetime import datetime
 
 def get_current_time():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_valid_questions(questions, search_phrase, key):
+    for question in questions:
+        if search_phrase in question[key]:
+            question[key] = question[key].replace(search_phrase,
+                                                  '<span id="search_phrase">' + search_phrase + '</span>')
